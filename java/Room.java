8ce7393bcf2afd1e0f31071a960bf531051e9bd8
Aravind Krishna A V <93380300+AravindKAV@users.noreply.github.com>
2022-03-25 10:22:53 +0530

    Add files via upload

---
+++ java/Room.java
@@ -0,0 +1,18 @@
+class Room
+{
+	float length;
+	float breadth;
+	Room(float x,float y)
+	{
+		length=x;
+		breadth=y;
+	}
+	Room(float x)
+	{
+		length=breadth=x;
+	}
+	int area()
+	{
+		return(length*breadth);
+	}
+}
